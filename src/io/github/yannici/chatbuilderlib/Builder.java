8b089fb04a535921e183f325c9506ac6a1cb415d
Yannici <yannicschne@gmail.com>
2015-05-07 07:34:06 +0200

    [1.0.1] Removed test-code

--- src/io/github/yannici/chatbuilderlib/Builder.java
+++ src/io/github/yannici/chatbuilderlib/Builder.java
@@ -1,6 +1,5 @@
 package io.github.yannici.chatbuilderlib;
 
-import io.github.yannici.chatbuilderlib.chat.elements.ChatTranslateElement;
 import io.github.yannici.chatbuilderlib.chat.events.ChatClickEventAction;
 import io.github.yannici.chatbuilderlib.chat.events.ChatHoverEventAction;
 
@@ -29,13 +28,6 @@ public class Builder extends JavaPlugin implements Listener {
         Builder.instance = this;
         
         this.loadVersion();
-        
-        ChatBuilder builder = new ChatBuilder();
-        ChatTranslateElement translate = (ChatTranslateElement)builder.appendTranslate("multiplayer.player.joined");
-        translate.addWith("Yannici");
-        translate.addWith(4);
-        
-        this.getServer().getConsoleSender().sendMessage(builder.getJson());
     }
     
     @Override
